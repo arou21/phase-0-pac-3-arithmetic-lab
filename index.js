99abacdffcb277b877118d6f0e3f7ddd427efeea
Samuel <smarouba2@gmail.com>
2022-04-04 09:50:04 -0500

    commit tree a4f172596f17da4c417780086b2ae0dc12c4c5bd

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a+b;
+    }
+    function subtract(a, b) {
+        return a-b;
+    }
+    function multiply(a, b) {
+        return a*b;
+    } 
+    function divide(a,b) {
+        return a/b;
+    }
+    function increment(a){
+    a++
+    return a;
+    }
+function decrement (c){
+c-- 
+return c;
+}
+function makeInt(n){  
+return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+// // }
